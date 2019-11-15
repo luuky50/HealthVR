0e79c9dfcff88fb51d5a3aa6704383f0d65805fd
Noah <noahpbrinkman@gmail.com>
2019-11-15 13:26:09 +0100

    Added Input Testing

---
+++ HealthVR/Assets/4. Scripts/GetInput.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetInput : MonoBehaviour
+{
+	void Update()
+	{
+		Debug.Log(Input.GetJoystickNames());
+	}
+}
