776aa5ba85da7e0ab387ff8613d2bcb97e40ae55
Mathias Bevers <bevertje01@icloud.com>
2019-11-18 11:25:47 +0100

    Added pickupable script
    This script will handle its type, if in a drop zone and which drop zone.

---
+++ HealthVR/Assets/4. Scripts/Pickupable.cs
@@ -0,0 +1,22 @@
+﻿using Valve.VR;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pickupable : MonoBehaviour
+{
+	private bool inDropZone = false;
+
+	[Header("Debug")]
+	[SerializeField] private SteamVR_Action_Boolean actionBinding;
+
+	private void OnEnable()
+	{
+		actionBinding.onStateUp += OnTriggerRelease;
+	}
+
+	private void OnTriggerRelease(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+	{
+		Debug.Log($"{fromSource}, {fromAction}");
+	}
+}
