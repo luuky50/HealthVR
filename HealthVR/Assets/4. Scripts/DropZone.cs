958bfff7f8503d31f574b5498ec6fe7703e20c74
Mathias Bevers <bevertje01@icloud.com>
2019-11-20 15:57:31 +0100

    Updated snapping logic

--- HealthVR/Assets/4. Scripts/DropZone.cs
+++ HealthVR/Assets/4. Scripts/DropZone.cs
@@ -6,21 +6,28 @@ public class DropZone : MonoBehaviour
 	//TODO: Check for pickup able type.
 	public bool IsOccupied { get; private set; }
 	private Transform cachedTransform = null;
+	private Pickupable occupyingObject = null;
 
 	private void OnEnable()
 	{
 		cachedTransform = GetComponent<Transform>();
 	}
 
-	public void Occupy(Transform dropedObject)
+	public void Occupy(Pickupable occupyingObject)
 	{
 		if (IsOccupied)
 		{			
 			return;
 		}
 
+		Debug.Log($"Ocupping {name}...");
 		IsOccupied = true;
-		dropedObject.transform.position = cachedTransform.position;
+
+		occupyingObject.InDropZone = true;
+		occupyingObject.transform.position = cachedTransform.position;
+		occupyingObject.GetComponent<Rigidbody>().isKinematic = true;
+
+		this.occupyingObject = occupyingObject;
 	}
 
 	public void Unoccupy()
@@ -30,6 +37,10 @@ public class DropZone : MonoBehaviour
 			return;
 		}
 
+		Debug.Log($"Unocupping {name}...");
 		IsOccupied = false;
+
+		occupyingObject.InDropZone = false;
+		occupyingObject.GetComponent<Rigidbody>().isKinematic = false;
 	}
 }
