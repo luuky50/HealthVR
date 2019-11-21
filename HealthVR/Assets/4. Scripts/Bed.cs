505ad9f4c13783afc340afc5a4a3eb06702e1975
Noah <noahpbrinkman@gmail.com>
2019-11-21 13:27:52 +0100

    Bed logic rework

---
+++ HealthVR/Assets/4. Scripts/Bed.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+
+	[SerializeField] private Transform turningPoint;
+	[SerializeField] private Transform moveInPoint;
+	[SerializeField] private Transform moveOutPoint;
+
+	public void MoveOut()
+	{
+		LeanTween.move(gameObject, turningPoint.position, 2);
+		LeanTween.rotateY(gameObject, 0, 1);
+		gameObject.SetActive(false);
+	}
+
+	public void MoveIn()
+	{
+		LeanTween.move(gameObject, turningPoint.position, 2);
+		LeanTween.rotateY(gameObject, 90, 1);
+	}
+
+
+}
