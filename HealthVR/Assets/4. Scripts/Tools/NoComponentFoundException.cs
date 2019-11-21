1951883dceea6e4e6427075d75609fa1071eb55c
Mathias Bevers <bevertje01@icloud.com>
2019-11-21 19:14:50 +0100

    Added MonoBehaviourExtra

---
+++ HealthVR/Assets/4. Scripts/Tools/NoComponentFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniFix
+{
+	public class NoComponentFoundException : Exception
+	{
+		public NoComponentFoundException(string componentName) : base(string.Format($"There is no {componentName} component found."))
+		{
+
+		}
+	}
+}
